7672bebc34309dfb47f8aa4f64b9d4df783fec3b
Paul Schulten <paul_schulten@yahoo.com>
2026-07-27 11:33:28 -0700

    Add Glue catalog support alongside local SQLite (LAKEHOUSE_ENV toggle)

--- 01_pipelines/iceberg_catalog.py
+++ 01_pipelines/iceberg_catalog.py
@@ -1,7 +1,20 @@
+
+import os
 from pathlib import Path
 from pyiceberg.catalog.sql import SqlCatalog
+from pyiceberg.catalog.glue import GlueCatalog
+
 
 def get_catalog():
+    if os.environ.get("LAKEHOUSE_ENV") == "aws":
+        return GlueCatalog(
+            "default",
+            **{
+                "warehouse": "s3://la-homelessness-lakehouse-277607772876-us-east-2-an",
+                "region_name": "us-east-2",
+            },
+        )
+
     warehouse_path = Path(__file__).parent.parent / "02_data" / "01_iceberg"
     return SqlCatalog(
         "default",
